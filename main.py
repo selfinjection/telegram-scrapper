baaf14464cffdd1bb944d5fd5ae0aa1451c9be75
trace <yoyort21@protonmail.com>
2023-01-25 00:18:03 +0000

    init

--- main.py
+++ main.py
@@ -19,20 +19,28 @@ def get_groups(dialogs):
             groups.append(dialog)
     return groups
 
+def find_message(username):
+    pass
 
 def main():
-    with open('config/config.json') as file:
+    with open('config/config.json', 'r') as file:
         data = json.load(file)
+
     id, hash, phone = data['380939793299']['id'], data['380939793299']['hash'], data['380939793299']['phone']
     client = TelegramClient(phone, id, hash)
+
     client.connect()
     if not client.is_user_authorized():
         client.send_code_request(phone)
         client.sign_in(phone, input('[+] Enter the verification code: '))
     
     dialogs = client.get_dialogs()
+
     groups = get_groups(dialogs)
     msgs = get_messages(client, groups[0].title, limit=10)
+    practicipants = client.get_participants(groups[0])
 
+    with open('messages.json', 'w', encoding='utf-8') as file:
+        json.dump(msgs, file, indent=4, ensure_ascii=False)
 
 main()
